32a4e97ebb2d97e40d856bcdb9b384d3e539f60e
Blaouwu <bloodyblao@gmail.com>
2025-10-03 20:15:57 +0530

    Fix backend db 3rd time

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("dns_db", "root", "1245", {
+const sequelize = new Sequelize("db", "root", "1245", {
   host: "127.0.0.1",
   dialect: "mysql",
   // logging: console.log,
