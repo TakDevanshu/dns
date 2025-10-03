f341405b795037e759cfb6cc1c6a0424e34f1345
Blaouwu <bloodyblao@gmail.com>
2025-10-03 20:37:47 +0530

    host name change

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("dns_db", "root", "1245", {
-  host: "127.0.0.1",
+  host: "db",
   dialect: "mysql",
   // logging: console.log,
   logging: false,
